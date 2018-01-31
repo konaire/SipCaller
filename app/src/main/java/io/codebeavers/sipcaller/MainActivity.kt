f913700391f547f2a59b97ed32a277b474e88525
Evgeny Eliseyev <eugvelis@gmail.com>
2018-01-31 16:12:45 +0300

    initial commit

---
+++ app/src/main/java/io/codebeavers/sipcaller/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.codebeavers.sipcaller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Evgeny Eliseyev on 31/01/2018.
+ */
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
